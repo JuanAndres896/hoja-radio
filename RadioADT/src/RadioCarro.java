c8a71d039c83edfc87d3096cac572c3567a7e370
Christian Morales <Christian Morales@172.20.13.211>
2016-07-06 16:59:11 -0600

    Segunda modificacion RadioADT

--- RadioADT/src/RadioCarro.java
+++ RadioADT/src/RadioCarro.java
@@ -8,54 +8,93 @@
  *
  * @author Christian Morales
  */
-public interface Radio {
+public class RadioCarro implements Radio{
+    
+    /**
+     * Atributo para manejar el estado de encendido/apagado de la radio (true/false respectivamente)
+     */
+    public boolean encendido;
+    
+    /**
+        * Atributo para manejar la elección de frecuencia am/fm (false/true respectivamente)
+     */
+    public boolean frecuencia;
+    
+    /** 
+     * Atributo para guardar la elección de la estación 
+     */
+    public double estacion;
     
     /**
      * Método para para manejar el estado de encendido/apagado de la radio (true/false respectivamente)
      * @param estado boolean para indicar el estado de encendido/apagado de la radio
      */
-    public void setEncendido(boolean estado);
+    @Override
+    public void setEncendido(boolean estado){
+        
+    }
     
     /**
      * Método para manejar la elección de frecuencia am/fm (false/true respectivamente)
      * @param frec boolean para indicar la elección de frecuencia am/fm 
      */
-    public void setFrecuencia(boolean frec);
+    @Override
+    public void setFrecuencia(boolean frec){
+        
+    }
     
     /**
      * Método para ir cambiando las estaciones
      * @param est double para guardar la elección de la estación
      */
-    public void setEstacion(double est);
+    @Override
+    public void setEstacion(double est){
+        
+    }
     
     /**
      * Método para guardar una emisora favorita
      * @param em int para alojar la emisora en un botón
      */
-    public void guardarEmisora(int em);
+    @Override
+    public void guardarEmisora(int em){
+        
+    }
     
     /**
      * Método para seleccionar una de las emisoras puesta en el botón
      * @param emi int para identificar la emisora en el botón y seleccionarla
      */
-    public void seleccionarEmisora(int emi);
+    @Override
+    public void seleccionarEmisora(int emi){
+        
+    }
     
     /**
      * Método que devuelve el estado de encendido/apagado de la radio
      * @return encendido: boolean del estado
      */
-    public boolean getEncendido();
+    @Override
+    public boolean getEncendido(){
+        return encendido;
+    }
     
     /**
      * Método que devuelve la frecuencia seleccionada
      * @return frecuencia: boolean de am/fm
      */
-    public boolean getFrecuencia();
+    @Override
+    public boolean getFrecuencia(){
+        return frecuencia;
+    }
     
     /**
      * Método que devuelve la estación seleccionada
      * @return double de la estación que se escucha 
      */
-    public double getEstacion();   
+    @Override
+    public double getEstacion(){
+        return estacion;
+    }   
     
 }
