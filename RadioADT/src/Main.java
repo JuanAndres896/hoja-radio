43af7271573df235680861f2c8c9fc816f0eb9be
jose luis <jose luis@joseluismendez>
2016-07-09 12:18:19 -0600

    modificacion de gui

--- RadioADT/src/Main.java
+++ RadioADT/src/Main.java
@@ -47,151 +47,114 @@ public class Main extends javax.swing.JFrame {
         jBguardar = new javax.swing.JButton();
         jBsubirEmisora = new javax.swing.JButton();
         jBbajarEmisora = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(550, 450));
+        setResizable(false);
+        getContentPane().setLayout(null);
 
         jB1.setText("1");
+        getContentPane().add(jB1);
+        jB1.setBounds(47, 154, 45, 23);
 
         jB4.setText("4");
+        getContentPane().add(jB4);
+        jB4.setBounds(47, 183, 45, 23);
 
         jB7.setText("7");
+        getContentPane().add(jB7);
+        jB7.setBounds(47, 217, 45, 23);
 
         jB10.setText("10");
+        getContentPane().add(jB10);
+        jB10.setBounds(47, 246, 45, 23);
 
         jB2.setText("2");
+        getContentPane().add(jB2);
+        jB2.setBounds(110, 154, 45, 23);
 
         jB5.setText("5");
+        getContentPane().add(jB5);
+        jB5.setBounds(110, 183, 45, 23);
 
         jB8.setText("8");
+        getContentPane().add(jB8);
+        jB8.setBounds(110, 217, 45, 23);
 
         jB11.setText("11");
+        getContentPane().add(jB11);
+        jB11.setBounds(110, 246, 45, 23);
 
         jB9.setText("9");
+        getContentPane().add(jB9);
+        jB9.setBounds(167, 217, 45, 23);
 
         jB12.setText("12");
+        getContentPane().add(jB12);
+        jB12.setBounds(165, 246, 47, 23);
 
         jB3.setText("3");
+        getContentPane().add(jB3);
+        jB3.setBounds(167, 154, 45, 23);
 
         jB6.setText("6");
+        getContentPane().add(jB6);
+        jB6.setBounds(167, 183, 45, 23);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("----.---");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(47, 61, 116, 43);
 
+        jRadioButton1.setForeground(new java.awt.Color(255, 255, 255));
         jRadioButton1.setText("AM");
+        getContentPane().add(jRadioButton1);
+        jRadioButton1.setBounds(163, 61, 41, 23);
 
+        jRadioButton2.setForeground(new java.awt.Color(255, 255, 255));
         jRadioButton2.setText("FM");
+        getContentPane().add(jRadioButton2);
+        jRadioButton2.setBounds(163, 95, 39, 23);
 
-        jBpower.setText("on/off");
+        jBpower.setIcon(new javax.swing.ImageIcon(getClass().getResource("/power1.png"))); // NOI18N
+        jBpower.setBorderPainted(false);
+        jBpower.setContentAreaFilled(false);
+        jBpower.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/power2.png"))); // NOI18N
+        getContentPane().add(jBpower);
+        jBpower.setBounds(250, 330, 70, 60);
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Guardar Emisora.", "1.", "2.", "3.", "4.", "5.", "6.", "7.", "8.", "9.", "11.", "12." }));
         jComboBox1.setToolTipText("");
+        getContentPane().add(jComboBox1);
+        jComboBox1.setBounds(318, 155, 150, 20);
 
-        jBguardar.setText("guardar");
+        jBguardar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guardar1.png"))); // NOI18N
+        jBguardar.setBorderPainted(false);
+        jBguardar.setContentAreaFilled(false);
+        jBguardar.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/guardar2.png"))); // NOI18N
+        getContentPane().add(jBguardar);
+        jBguardar.setBounds(350, 190, 90, 80);
 
-        jBsubirEmisora.setText("+");
+        jBsubirEmisora.setIcon(new javax.swing.ImageIcon(getClass().getResource("/+1.png"))); // NOI18N
+        jBsubirEmisora.setBorderPainted(false);
+        jBsubirEmisora.setContentAreaFilled(false);
+        jBsubirEmisora.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/+2.png"))); // NOI18N
+        getContentPane().add(jBsubirEmisora);
+        jBsubirEmisora.setBounds(330, 70, 40, 43);
 
-        jBbajarEmisora.setText("-");
+        jBbajarEmisora.setIcon(new javax.swing.ImageIcon(getClass().getResource("/-1.png"))); // NOI18N
+        jBbajarEmisora.setBorderPainted(false);
+        jBbajarEmisora.setContentAreaFilled(false);
+        jBbajarEmisora.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/-2.png"))); // NOI18N
+        getContentPane().add(jBbajarEmisora);
+        jBbajarEmisora.setBounds(380, 70, 50, 40);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(47, 47, 47)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(jB10, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(jB1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(jB7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                    .addComponent(jB4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(18, 18, 18)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jB11)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(jB12, javax.swing.GroupLayout.DEFAULT_SIZE, 47, Short.MAX_VALUE))
-                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                            .addComponent(jB5, javax.swing.GroupLayout.DEFAULT_SIZE, 45, Short.MAX_VALUE)
-                                            .addComponent(jB2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                        .addGap(12, 12, 12)
-                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                            .addComponent(jB6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                            .addComponent(jB3, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jB8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addGap(12, 12, 12)
-                                        .addComponent(jB9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addGap(106, 106, 106)
-                                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addGap(124, 124, 124)
-                                        .addComponent(jBguardar))))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jRadioButton1)
-                                    .addComponent(jRadioButton2))
-                                .addGap(92, 92, 92)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jBsubirEmisora, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jBbajarEmisora, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(230, 230, 230)
-                        .addComponent(jBpower)))
-                .addContainerGap(50, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(61, 61, 61)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jRadioButton1)
-                            .addComponent(jBsubirEmisora))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jRadioButton2)
-                            .addComponent(jBbajarEmisora))))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(36, 36, 36)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jB1)
-                            .addComponent(jB2)
-                            .addComponent(jB3)
-                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jB4)
-                            .addComponent(jB5)
-                            .addComponent(jB6))
-                        .addGap(11, 11, 11)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jB7)
-                            .addComponent(jB8)
-                            .addComponent(jB9)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(77, 77, 77)
-                        .addComponent(jBguardar)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jB10)
-                    .addComponent(jB11)
-                    .addComponent(jB12))
-                .addGap(40, 40, 40)
-                .addComponent(jBpower)
-                .addContainerGap(58, Short.MAX_VALUE))
-        );
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/fondo main.jpg"))); // NOI18N
+        jLabel2.setText("jLabel2");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(0, 0, 560, 450);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -251,6 +214,7 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JButton jBsubirEmisora;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JRadioButton jRadioButton1;
     private javax.swing.JRadioButton jRadioButton2;
     // End of variables declaration//GEN-END:variables
